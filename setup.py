894872b66520d9d61342cc982eee07f7e7ee1de4
jim <jim@jimtufts.com>
2021-07-15 20:11:31 -0500

    Remove util build instructions from main setup.py, test fix imports in grids.py

--- setup.py
+++ setup.py
@@ -57,23 +57,3 @@ setup(
     # zip_safe=False,
 
 )
-
-try:
-    from setuptools import setup
-    from setuptools import Extension
-    from Cython.Build import cythonize
-except ImportError:
-    from distutils.core import setup
-    from distutils.extension import Extension
-
-from Cython.Distutils import build_ext
-import numpy as np
-
-ext_modules = [Extension("util",["util.pyx"])]
-
-setup(
-    name= 'util',
-    cmdclass = {'build_ext': build_ext},
-    include_dirs = [np.get_include()],
-    ext_modules = ext_modules,
-    )
